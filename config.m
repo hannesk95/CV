f4c797abd73568f4ca54a83b767f8c5d9717cca0
Patrick Blümcke <patrick.bluemcke@tum.de>
2020-07-07 17:13:14 +0200

    Update config.m, set bg-default as 0

--- config.m
+++ config.m
@@ -5,7 +5,7 @@
 group_number = 4;
 
 % Group members:
-members = {'Hacket Franziska', 'Blümcke Patrick', 'Muhr Florian', 'Stümke Daniel', 'Kiechle Johannes'};
+members = {'Hacket Franziska', 'BlÃ¼mcke Patrick', 'Muhr Florian', 'StÃ¼mke Daniel', 'Kiechle Johannes'};
 
 % Email-Address (from Moodle!):
 mail = {'franziska.hacket@tum.de', 'patrick.bluemcke@tum.de', 'daniel.stuemke@tum.de', 'f.muhr@tum.de'};
@@ -16,14 +16,14 @@ mail = {'franziska.hacket@tum.de', 'patrick.bluemcke@tum.de', 'daniel.stuemke@tu
 src = "Path/to/my/ChokePoint/P1E_S1";
 
 % Select Cameras
-L = 1
-R = 2
+L = 1;
+R = 2;
 
 % Choose a start point
-start = 1
+start = 1;
 
 % Choose the number of succseeding frames
-N = 3
+N = 3;
 
 ir = ImageReader(src, L, R, start, N);
 
@@ -33,13 +33,11 @@ ir = ImageReader(src, L, R, start, N);
 dst = "output.avi";
 
 % Load Virual Background
+bg = 0;
 % bg = imread("Path\to\my\virtual\background")
 
 % Select rendering mode
 mode = "substitute";
-
-% Create a movie array
-% movie =
 
 % Store Output?
 store = true;
