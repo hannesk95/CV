78d694d52f15ec7b6d3c9ef6f260f8ccdc86e928
Daniel Stümke <ge37bov@mytum.de>
2020-07-11 13:11:40 +0200

    Update segmentation.m

--- segmentation.m
+++ segmentation.m
@@ -1,8 +1,11 @@
-function mask = segmentation(tensor_left, tensor_right)
-    [tensor_l_out, tensor_l_scaled, scaling_factor] = prepare_images(tensor_left);
+function mask = segmentation(left, right)
+    % Convert images to grayscale and scale to desired size
+    [tensor_l_out, tensor_l_scaled, scaling_factor] = prepare_images(left);
 
+    % Search for a region of interest
     rois = find_roi(tensor_l_scaled, scaling_factor);
 
+    % Calculate foreground mask using the left images and the region of interest
     mask = generate_mask(tensor_l_out, rois);
 end
 
