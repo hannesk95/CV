62504b0f12f539d7e846662037348acc05f709a0
Patrick Blümcke <patrick.bluemcke@tum.de>
2020-07-11 13:33:32 +0200

    Update generate_ellipse.m

--- generate_ellipse.m
+++ generate_ellipse.m
@@ -1,6 +1,6 @@
 function mask = generate_ellipse(width, height)
-    parabel1 = (linspace(0, 2, height) - 1) .^ 2;
-    parabel2 = (linspace(0, 2, width) - 1) .^ 2;    
-    mask = parabel1' + parabel2 <= 1;
+    parabola1 = (linspace(0, 2, height) - 1) .^ 2;
+    parabola2 = (linspace(0, 2, width) - 1) .^ 2;    
+    mask = parabola1' + parabola2 <= 1;
 end
 
